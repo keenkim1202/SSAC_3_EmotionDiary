0063c028874dfcab1e3ab758053896ea499d6cf0
KEEN <homurahomu@naver.com>
2021-10-07 11:01:45 +0900

    UI 구성

---
+++ EmotionDiary/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  EmotionDiary
+//
+//  Created by KEEN on 2021/10/06.
+//
+
+import UIKit
+import TextFieldEffects
+
+class MainViewController: UIViewController {
+
+  // MARK: UI
+
+  // MARK: View Life-Cycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+  // MARK: Actions
+
+  
+}
+
